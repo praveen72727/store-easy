e999881372f29f5a2e41c582f30568121fa32da2
praveen72727 <131275461+praveen72727@users.noreply.github.com>
2024-10-21 18:41:24 +0530

    Removed Angular code from the project

--- src/app/pages/page-not-found/page-not-found.routers.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found.component';
-
-export const pageNotFoundRouters: Routes = [{ path: 'page-not-found', component: PageNotFoundComponent }];
